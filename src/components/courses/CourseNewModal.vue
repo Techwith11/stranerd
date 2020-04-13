cd7bbc89b9bb132cc4c6edbafd61d7704f9add6d
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-04-13 20:31:25 +0100

    Added image preview field to course create modal

--- src/components/courses/CourseNewModal.vue
+++ src/components/courses/CourseNewModal.vue
@@ -14,10 +14,17 @@
 					>
 				</div>
 				<div class="my-3">
-					<input type="file" @change="catchVideo" class="d-none" ref="videoInput">
+					<input type="file" @change="catchVideo" class="d-none" ref="videoInput" accept="video/*">
 					<span @click="$refs.videoInput.click()">
-						<span>{{file.name}} </span>
-						<span class="text-info">Choose file</span>
+						<span>{{video.name}} </span>
+						<span class="text-info">Choose video</span>
+					</span>
+				</div>
+				<div class="my-3">
+					<input type="file" @change="catchImage" class="d-none" ref="imageInput"  accept="image/*">
+					<span @click="$refs.imageInput.click()">
+						<span>{{image.name}} </span>
+						<span class="text-info">Choose image preview</span>
 					</span>
 				</div>
 				<div class="form-group">
@@ -31,7 +38,7 @@
 					<label class="custom-control-label" for="isPremium">Is premium?</label>
 				</div>
 				<div  v-if="course.premium" class="my-3">
-					<input type="file" @change="catchPreview" class="d-none" ref="previewInput">
+					<input type="file" @change="catchPreview" class="d-none" ref="previewInput"  accept="video/*">
 					<span @click.prevent="$refs.previewInput.click()">
 						<span>{{preview.name}}  </span>
 						<span class="text-info">Upload preview</span>
@@ -52,16 +59,19 @@
 				description: '',
 				premium: false,
 			},
-			file: {},
+			video: {},
+			image: {},
 			preview: {},
 		}),
 		methods: {
 			closeModal(){ window.Fire.$emit('closeCourseNewModal') },
-			catchVideo(e){ this.file = e.target.files[0] },
-			catchPreview(e){ this.preview = e.target.files[0] },
+			catchVideo(e){ e.target.files[0].type.startsWith('video/') ? this.video = e.target.files[0] : null},
+			catchImage(e){ e.target.files[0].type.startsWith('image/') ? this.image = e.target.files[0] : null},
+			catchPreview(e){ e.target.files[0].type.startsWith('video/') ? this.preview = e.target.files[0] : null},
 			createCourse(){
 				if(!this.$v.$error && !this.$v.invalid){
 					// TODO: Upload video and append storage url to course object
+					// TODO: Upload image and append storage url to course object
 					// TODO: Upload preview and append storage url to course object
 					// TODO: Create course in firebase
 					// TODO: Close modal and redirect to course page
