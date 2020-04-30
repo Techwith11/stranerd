d27db618ab09c3a62dbc5b0ab4e795316ecf1616
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-04-30 08:31:23 +0100

    Changed font from raleway to poppins

---
+++ functions/onCall/markTutorTest.js
@@ -0,0 +1,10 @@
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+
+module.exports = functions.https.onCall(async (data, context) => {
+	if (!context.auth) {
+		/*TODO: Delete comment */ //throw new functions.https.HttpsError('unauthenticated', 'Only authenticated users can send messages')
+	}
+
+	return true
+})
