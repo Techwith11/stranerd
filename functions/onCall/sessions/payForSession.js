377962ce4a387bea1d954c5b78447cf5f17dae67
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-05-08 23:14:27 +0100

    Moved end time of session from acceptSession to payForSession

--- functions/onCall/sessions/payForSession.js
+++ functions/onCall/sessions/payForSession.js
@@ -3,22 +3,29 @@ const admin = require('firebase-admin')
 
 module.exports = functions.https.onCall(async (data, context) => {
 	if (!context.auth) {
-		/*TODO: Delete comment */ //throw new functions.https.HttpsError('unauthenticated', 'Only authenticated users can accept sessions')
+		/*TODO: Delete comment */ //throw new functions.https.HttpsError('unauthenticated', 'Only authenticated users can pay for sessions')
 	}
 
-	let ref = admin.firestore().collection('sessions').doc(data)
+	let ref = admin.firestore().collection('sessions').doc(data.id)
 
 	let session = (await ref.get()).data()
 
-	if (context.auth && context.auth.uid !== session.tutor) {
-		/*TODO: Delete comment */ //throw new functions.https.HttpsError('unauthenticated', 'Only the tutor of the session can accept it')
+	if (context.auth && context.auth.uid !== session.student) {
+		/*TODO: Delete comment */ //throw new functions.https.HttpsError('unauthenticated', 'Only the student of the session can pay for it')
 	}
 
 	let endedAt = admin.firestore.Timestamp.now().toDate()
 	endedAt.setMinutes(endedAt.getMinutes() + 60 * session.duration)
 
-	return await ref.set({
-		accepted: true,
+	let charge = data.charge
+	charge.dates = { createdAt: admin.firestore.FieldValue.serverTimestamp() }
+
+	await admin.firestore().collection(`users/${session.student}/charges`).add(charge)
+
+	let update = {
 		dates: { endedAt }
-	}, { merge: true })
+	}
+	if(charge.success){ update.paid = true }
+
+	return await ref.set(update, { merge: true })
 })
