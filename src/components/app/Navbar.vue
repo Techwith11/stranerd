ef83f4047008d9260c137c69a9618cce1c546cc9
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-05-10 02:18:30 +0100

    Showing different navbars on login

--- src/components/app/Navbar.vue
+++ src/components/app/Navbar.vue
@@ -1,15 +1,12 @@
 <template>
     <header class="bg-dark">
-        <nav class="container navbar navbar-expand-md navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">STRANERD</a>
+        <nav class="container navbar navbar-dark bg-dark" :class="isLoggedIn ? 'navbar-expand-md' : 'navbar-expand-sm'">
+            <router-link class="navbar-brand" to="/">STRANERD</router-link>
             <button class="navbar-toggler shadow-none rounded-0" @click="toggleNavbar" type="button" data-toggle="collapse" data-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse text-center">
-                <ul class="navbar-nav ml-auto align-items-md-center">
-                    <li class="nav-item">
-                        <router-link class="nav-link" to="/">Home</router-link>
-                    </li>
+                <ul class="navbar-nav ml-auto align-items-md-center" v-if="isLoggedIn">
                     <li class="nav-item">
                         <router-link class="nav-link" to="/chats">Chats</router-link>
                     </li>
@@ -22,28 +19,29 @@
                     <li class="nav-item">
                         <router-link class="nav-link" to="/tutors">Tutors</router-link>
                     </li>
-                    <template v-if="isLoggedIn">
-                        <li class="nav-item dropdown d-none d-md-inline">
-                            <a class="nav-link dropdown-toggle" href="#" @click="toggleDropDown" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                My account
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <router-link class="dropdown-item" to="/my_account">My Account</router-link>
-                                <router-link class="dropdown-item" to="/cart">My cart</router-link>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item"  @click.prevent="logout">Logout</a>
-                            </div>
-                        </li>
-                        <li class="nav-item d-md-none">
-                            <router-link class="nav-link" to="/my_account">My Account</router-link>
-                        </li>
-                        <li class="nav-item d-md-none">
-                            <a class="nav-link" @click.prevent="logout">Logout</a>
-                        </li>
-                    </template>
-                    <li class="nav-item" v-else>
-                        <button class="primary-button d-none d-md-inline" @click="openModal">Get Started</button>
-                        <a class="nav-link d-md-none" @click.prevent="openModal">Get Started</a>
+                    <li class="nav-item dropdown d-none d-md-inline">
+                        <a class="nav-link dropdown-toggle" href="#" @click="toggleDropDown" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            My account
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <router-link class="dropdown-item" to="/my_account">My Account</router-link>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item"  @click.prevent="logout">Logout</a>
+                        </div>
+                    </li>
+                    <li class="nav-item d-md-none">
+                        <router-link class="nav-link" to="/my_account">My Account</router-link>
+                    </li>
+                    <li class="nav-item d-md-none">
+                        <a class="nav-link" @click.prevent="logout">Logout</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ml-auto align-items-md-center" v-else>
+                    <li class="nav-item d-inline">
+                        <a href="#" class="nav-link" @click.prevent="openModal">Sign Up</a>
+                    </li>
+                    <li class="nav-item d-inline">
+                        <a href="#" class="nav-link" @click.prevent="openModal">Login</a>
                     </li>
                 </ul>
             </div>
@@ -55,7 +53,7 @@
 	header{
         position: relative;
 		z-index: 3;
-		height: 60px;
+		height: 68px;
 	}
 </style>
 
