a70c97a526e38a69f1fdc59dc8a8f0f52cffab1b
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-05-14 03:21:56 +0100

    Created createPaymentMethod onCall function

--- functions/onCall/payments/createPaymentMethod.js
+++ functions/onCall/payments/createPaymentMethod.js
@@ -3,9 +3,13 @@ const admin = require('firebase-admin')
 const braintree = require('braintree')
 
 module.exports = functions.https.onCall(async (data, context) => {
-	/*if (!context.auth) {
-		/!*TODO: Delete comment *!/ //throw new functions.https.HttpsError('unauthenticated', 'Only authenticated users can invoke payments')
-	}*/
+	if (!context.auth) {
+		/!*TODO: Delete comment *!/ //throw new functions.https.HttpsError('unauthenticated', 'Only authenticated users can create payment methods')
+	}
+	let ref = await admin.firestore().collection('users').doc(data.id).get()
+	if(!ref.exists){ throw new functions.https.HttpsError('invalid-argument', 'User doesn\'t exist') }
+	let customerId = ref.data().account.customer_id
+	if(!customerId){ throw new functions.https.HttpsError('invalid-argument', 'User doesn\'t have a valid customer id') }
 	try{
 		let gateway = braintree.connect({
 			environment: braintree.Environment.Sandbox,
@@ -13,10 +17,15 @@ module.exports = functions.https.onCall(async (data, context) => {
 			publicKey: 'c57yb9sm9fc79c9x',
 			privateKey: 'bb26a3a051b5d1da5f430872c820f616',
 		})
-		return await gateway.transaction.sale({
-			amount: data.amount,
-			paymentMethodNonce: data.nonce,
+		let result = await gateway.paymentMethod.create({
+			customerId,
+			paymentMethodNonce: data.nonce
 		})
+		if(result.success){
+			let details = JSON.parse(JSON.stringify(result.paymentMethod))
+			await admin.firestore().collection(`users/${data.id}/paymentMethods`).add(details)
+		}
+		return result.success
 	}catch(error){
 		throw new functions.https.HttpsError('unknown', error.message)
 	}
