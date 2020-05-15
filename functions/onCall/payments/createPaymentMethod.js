f1b4b9c248d650a05f55d6f1668f3c92cbef0abf
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-05-15 21:43:18 +0100

    Moved all braintree config keys to functions config

--- functions/onCall/payments/createPaymentMethod.js
+++ functions/onCall/payments/createPaymentMethod.js
@@ -11,12 +11,22 @@ module.exports = functions.https.onCall(async (data, context) => {
 	let customerId = ref.data().account.customer_id
 	if(!customerId){ throw new functions.https.HttpsError('invalid-argument', 'User doesn\'t have a valid customer id') }
 	try{
-		let gateway = braintree.connect({
-			environment: braintree.Environment.Sandbox,
-			merchantId: '6f9mkyr3h4k9gvjk',
-			publicKey: 'c57yb9sm9fc79c9x',
-			privateKey: 'bb26a3a051b5d1da5f430872c820f616',
-		})
+		let gateway = null
+		if(functions.config().braintree.environment === 'live'){
+			gateway = braintree.connect({
+				environment: braintree.Environment.Production,
+				merchantId: functions.config().braintree.live.merchant_id,
+				publicKey: functions.config().braintree.live.public_key,
+				privateKey: functions.config().braintree.live.private_key
+			})
+		}else{
+			gateway = braintree.connect({
+				environment: braintree.Environment.Sandbox,
+				merchantId: functions.config().braintree.sandbox.merchant_id,
+				publicKey: functions.config().braintree.sandbox.public_key,
+				privateKey: functions.config().braintree.sandbox.private_key
+			})
+		}
 		let result = await gateway.paymentMethod.create({
 			customerId,
 			paymentMethodNonce: data.nonce
--- functions/onCall/payments/processPayment.js
+++ functions/onCall/payments/processPayment.js
@@ -7,12 +7,22 @@ module.exports = functions.https.onCall(async (data, context) => {
 		throw new functions.https.HttpsError('unauthenticated', 'Only authenticated users can invoke payments')
 	}
 	try{
-		let gateway = braintree.connect({
-			environment: braintree.Environment.Sandbox,
-			merchantId: '6f9mkyr3h4k9gvjk',
-			publicKey: 'c57yb9sm9fc79c9x',
-			privateKey: 'bb26a3a051b5d1da5f430872c820f616',
-		})
+		let gateway = null
+		if(functions.config().braintree.environment === 'live'){
+			gateway = braintree.connect({
+				environment: braintree.Environment.Production,
+				merchantId: functions.config().braintree.live.merchant_id,
+				publicKey: functions.config().braintree.live.public_key,
+				privateKey: functions.config().braintree.live.private_key
+			})
+		}else{
+			gateway = braintree.connect({
+				environment: braintree.Environment.Sandbox,
+				merchantId: functions.config().braintree.sandbox.merchant_id,
+				publicKey: functions.config().braintree.sandbox.public_key,
+				privateKey: functions.config().braintree.sandbox.private_key
+			})
+		}
 		return await gateway.transaction.sale({
 			amount: data.amount,
 			paymentMethodNonce: data.nonce,
