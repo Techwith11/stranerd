9156ed8aa0fe36f7db3db485f83fb3f6b36cbe4e
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-06-16 14:13:45 +0100

    Added register for service worker and notification for new content

---
+++ src/config/registerServiceWorker.js
@@ -0,0 +1,26 @@
+import { Workbox, messageSW } from 'workbox-window'
+
+if('serviceWorker' in navigator){
+	let wb = new Workbox('./service_worker.js')
+	let registration;
+	let showSkipWaitingPrompt = async () => {
+		let result = await new window.SweetAlert({
+			title: 'New content',
+			text: 'This page has new content. Would you like to download it?',
+			icon: 'info',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Yes'
+		})
+		if (result.value) {
+			wb.addEventListener('controlling', window.location.reload)
+			if(registration && registration.waiting){
+				await messageSW(registration.waiting, { type: 'SKIP_WAITING' })
+			}
+		}
+	}
+	wb.addEventListener('waiting', showSkipWaitingPrompt)
+	wb.addEventListener('externalwaiting', showSkipWaitingPrompt)
+	wb.register().then(reg => registration = reg)
+}
--- src/config/service_worker.js
+++ src/config/service_worker.js
@@ -1,1 +1,23 @@
-self.addEventListener('install',() => console.log('Installing'))
+let { core, expiration, precaching, routing, strategies } = self.workbox
+
+self.addEventListener('message', event => {
+	if(event.data && event.data.type === 'SKIP_WAITING'){
+		event.currentTarget.skipWaiting()
+	}
+})
+self.addEventListener('install', async event => {
+	let cacheName = core.cacheNames.runtime
+	let cache = await caches.open(cacheName)
+	event.waitUntil(cache.addAll(['/']))
+})
+
+precaching.precacheAndRoute(self.__precacheManifest || [])
+routing.registerRoute(({ request }) => request.destination === 'style', new strategies.StaleWhileRevalidate({ cacheName: 'css-cache' }))
+routing.registerRoute(({ request }) => request.destination === 'script', new strategies.StaleWhileRevalidate({ cacheName: 'js-cache' }))
+routing.registerRoute(({ request }) => request.destination === 'image', new strategies.CacheFirst({ cacheName: 'image-cache', plugins: [
+		new expiration.Plugin({
+			maxEntries: 50,
+			maxAgeSeconds: 7 * 24 * 60 * 60
+		})
+	]
+}))
