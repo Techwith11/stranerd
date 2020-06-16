b98f6418f682d54a25700740d0eb8ec1133d2999
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-06-16 23:22:00 +0100

    Register sw only in prod and remodelled update notify from modal to toast

--- src/config/registerServiceWorker.js
+++ src/config/registerServiceWorker.js
@@ -1,17 +1,16 @@
 import { Workbox, messageSW } from 'workbox-window'
 
-if(process.env.Node_ENV === 'production' && 'serviceWorker' in navigator){
+if(process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator){
 	let wb = new Workbox('./service_worker.js')
 	let registration;
 	let showSkipWaitingPrompt = async () => {
-		let result = await new window.SweetAlert({
-			title: 'New content',
-			text: 'This page has new content. Would you like to download it?',
-			icon: 'info',
-			showCancelButton: true,
-			confirmButtonColor: '#3085d6',
-			cancelButtonColor: '#d33',
-			confirmButtonText: 'Yes'
+		let result = await new window.Toast({
+			title: 'New content available',
+			position: 'bottom-end',
+			confirmButtonText:'Refresh',
+			showConfirmButton:true,
+			timer:8000,
+			toast:true
 		})
 		if (result.value) {
 			wb.addEventListener('controlling', window.location.reload)
@@ -20,6 +19,7 @@ if(process.env.Node_ENV === 'production' && 'serviceWorker' in navigator){
 			}
 		}
 	}
+	console.log(123)
 	wb.addEventListener('waiting', showSkipWaitingPrompt)
 	wb.addEventListener('externalwaiting', showSkipWaitingPrompt)
 	wb.register().then(reg => registration = reg)
