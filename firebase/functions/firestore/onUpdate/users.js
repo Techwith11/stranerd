78c9846eea61c4d3a5689d4ffbc3aa0722542f73
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-06-27 00:29:21 +0100

    Checking if change in bio or tutor to send to algolia for users onUpdate

--- firebase/functions/firestore/onUpdate/users.js
+++ firebase/functions/firestore/onUpdate/users.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions')
 const algoliaSearch = require('algoliasearch')
+const equal = require('deep-equal')
 const environment = functions.config().environment.mode
 const algolia = functions.config().algolia[environment]
 
@@ -7,12 +8,14 @@ module.exports = functions.firestore.document('/users/{id}').onUpdate(async (sna
 	try{
 		const client = algoliaSearch(algolia.app_id, algolia.api_key)
 		const index = client.initIndex('users')
-		let bio = snap.after.data().bio
-		let data = { objectId: snap.after.id, bio }
-		if(snap.after.data().roles.isTutor){
-			data['tutor'] = snap.after.data().tutor
+		if(!equal(snap.before.data().bio, snap.after.data().bio) || !equal(snap.before.data().tutor, snap.after.data().tutor)){
+			let bio = snap.after.data().bio
+			let data = { objectId: snap.after.id, bio }
+			if(snap.after.data().roles.isTutor){
+				data['tutor'] = snap.after.data().tutor
+			}
+			await index.saveObject(data)
 		}
-		index.saveObject(data)
 	}catch(error){
 		console.warn(error)
 	}
