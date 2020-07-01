ab37fed92862fc1ee886278b82c279d808328830
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-07-01 12:59:18 +0100

    Made space for courses without videos

--- src/components/courses/single/SingleVideo.vue
+++ src/components/courses/single/SingleVideo.vue
@@ -1,6 +1,7 @@
 <template>
 	<div class="container-md p-0 px-md-4 pt-1">
-		<video :src="getVideoLink" class="w-100 mb-0" controls></video>
+		<video :src="getVideoLink" class="w-100 mb-0" controls v-if="hasVideo"></video>
+		<img :src="getImageLink" alt="" class="w-100" v-else>
 		<h6 class="text-center">{{ course.title }}</h6>
 	</div>
 </template>
@@ -14,7 +15,9 @@
 			}
 		},
 		computed: {
-			getVideoLink(){ return this.course.video.link }
+			getVideoLink(){ return this.course.video.link },
+			getImageLink(){ return this.course.image.link },
+			hasVideo(){ return this.course.hasVideo }
 		}
 	}
 </script>
