576f0ad60a2041293116ae04f12829d311990a85
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-07-16 12:46:03 +0100

    Changed hero images

--- src/components/home/notLoggedIn/HomeCTA.vue
+++ src/components/home/notLoggedIn/HomeCTA.vue
@@ -25,7 +25,7 @@
                 </div>
                 <div class="carousel-item" data-interval="3000">
                     <img src="@/assets/images/slides_3.jpg" class="d-block w-100" alt="">
-                    <div class="carousel-caption text-dark">
+                    <div class="carousel-caption">
                         <h1 class="font-weight-bold cta">Get Access to Unlimited Educational Resources. Everywhere, Every time!</h1>
                         <p class="subhead">Premium access to loads of resources ranging from courses, tests e.t.c.</p>
                         <button class="primary-button ctaButton"><router-link class="text-decoration-none text-white" to="/courses">Get Access</router-link></button>
@@ -38,15 +38,24 @@
 
 <style lang="scss" scoped>
     img{
-        max-height: 90vh;
         min-height: 40vh;
     }
     .carousel-caption{
+        left: 0;
+        bottom: 0;
         height: 100%;
+        width: 100%;
         display: flex;
+        padding: 0 0.5rem;
         flex-direction: column;
         justify-content: center;
         align-items: center;
+        background: #00000044;
+    }
+    @media (min-width: 768px){
+        .carousel-caption{
+            padding: 0 8rem;
+        }
     }
     .ctaButton{
         background: $primary-dark;
