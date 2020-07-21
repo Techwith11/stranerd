54bf76684df35ab9aa3bc8c62bd1b705f3ab313e
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-07-21 19:41:47 +0100

    Added sidebar layout and navbar

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,43 @@
+<template>
+	<div id="sidebar" class="small">
+		<router-link class="d-flex flex-column align-items-center" to="/posts">
+			<img src="@/assets/images/sidebar/posts.svg" alt="">
+			<p>Posts</p>
+		</router-link>
+		<router-link class="d-flex flex-column align-items-center" to="/sessions">
+			<img src="@/assets/images/sidebar/sessions.svg" alt="">
+			<p>Sessions</p>
+		</router-link>
+		<router-link class="d-flex flex-column align-items-center" to="/courses">
+			<img src="@/assets/images/sidebar/courses.svg" alt="">
+			<p>Courses</p>
+		</router-link>
+		<router-link class="d-flex flex-column align-items-center" to="/tutors">
+			<img src="@/assets/images/sidebar/tutors.svg" alt="">
+			<p>Tutors</p>
+		</router-link>
+		<router-link class="d-flex flex-column align-items-center" to="/shop">
+			<img src="@/assets/images/sidebar/shop.svg" alt="">
+			<p>Shop</p>
+		</router-link>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	#sidebar{
+		padding: 2rem 0;
+		background: $primary-dark;
+		width: 80px;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	img{
+		width: 30px;
+		height: 30px;
+		margin: 0 0 0.25rem;
+	}
+	a{
+		color: $text-black;
+	}
+</style>
--- src/config/layouts.js
+++ src/config/layouts.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 
-import Default from '@/layouts/default.vue'
+import Default from '@/layouts/Default.vue'
+import WithSidebar from '@/layouts/WithSidebar.vue'
 
 Vue.component('Default', Default)
+Vue.component('WithSidebar', WithSidebar)
