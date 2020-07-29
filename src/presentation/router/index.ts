433603050bd85b6f2076a642b12da517060885d9
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-07-29 17:19:04 +0100

    Router and notifications

--- src/presentation/router/index.ts
+++ src/presentation/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
+import VueRouter, { Route } from 'vue-router'
 import routes from './routes.js'
+import { notify } from '@/config/notifications'
 import { closeNavbar, closeAccountDropdown, closeAdminDropdown } from '@/config'
 
 import store from '@/store/'
@@ -11,9 +12,9 @@ const router = new VueRouter({
 	mode: 'history',
 	base: process.env.BASE_URL,
 	routes,
-	scrollBehavior: async to => {
-		let y = store.getters.getScrollCache[to.fullPath] || 0
-		return new Promise((resolve) => {
+	scrollBehavior: async (to, from, position: { x: number, y: number } | void) => {
+		let y: number = store.getters.getScrollCache[to.fullPath] || 0
+		new Promise((resolve) => {
 			setTimeout(() => {
 				resolve({ x: 0, y })
 			}, 1)
@@ -21,14 +22,14 @@ const router = new VueRouter({
 	}
 })
 
-router.beforeEach((to, from, next) => {
+router.beforeEach(async (to: Route, from: Route, next: (to?: string) => void) => {
 	store.dispatch('setScrollCache', { page: from.fullPath, position: document.documentElement.scrollTop })
 	const requiresAuth = to.matched.some(route => route.meta.requiresAuth)
 	const requiresAdmin = to.matched.some(route => route.meta.requiresAdmin)
 	const isLoggedIn = store.getters.isLoggedIn
 	const isAdmin = store.getters.isAdmin
 	if (requiresAuth && !isLoggedIn) {
-		new window.Toast({ icon: 'error', 'title': 'Login to continue' })
+		await notify({ icon: 'error', 'title': 'Login to continue' })
 		store.dispatch('setAuthModalLogin')
 		store.dispatch('setIntendedRoute', to.fullPath)
 		return next('/')
---
+++ src/presentation/router/routes.ts
@@ -0,0 +1,188 @@
+export default [
+    {
+        path: '/',
+        name: 'Home',
+        component: () => import(/* webpackChunkName: "home" */ '@/router/views/home.vue')
+    },
+    /*{
+        path: '/about',
+        name: 'About',
+        component: () => import(/!* webpackChunkName: "about" *!/ '@/router/views/about')
+    },*/
+    {
+        path: '/pricing-plans',
+        name: 'Pricing Plan',
+        component: () => import(/* webpackChunkName: "pricing_plans" */ '@/router/views/pricing-plans.vue')
+    },
+    {
+        path: '/blog',
+        name: 'Blog Posts',
+        component: () => import(/* webpackChunkName: "blog_posts" */ '@/router/views/blog/index.vue')
+    },
+    {
+        path: '/blog/:id',
+        name: 'Blog Post',
+        component: () => import(/* webpackChunkName: "blog_post" */ '@/router/views/blog/single_blog.vue')
+    },
+    {
+        path: '/shop',
+        name: 'Shop',
+        component: () => import(/* webpackChunkName: "shop" */ '@/router/views/shop/index.vue'),
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/courses',
+        name: 'Courses',
+        component: () => import(/* webpackChunkName: "courses" */ '@/router/views/courses/index.vue'),
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/courses/:subject',
+        name: 'CourseSubject',
+        component: () => import(/* webpackChunkName: "course-subject" */ '@/router/views/courses/single-subject.vue'),
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/courses/:subject/:module',
+        name: 'CourseModule',
+        component: () => import(/* webpackChunkName: "course-module" */ '@/router/views/courses/single-module.vue'),
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/courses/:subject/:module/:id',
+        name: 'Course',
+        component: () => import(/* webpackChunkName: "course" */ '@/router/views/courses/single-course.vue'),
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/tutors',
+        name: 'Tutors',
+        component: () => import(/* webpackChunkName: "tutors" */ '@/router/views/tutors/index.vue'),
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/users/:id',
+        name: 'User',
+        component: () => import(/* webpackChunkName: "user" */ '@/router/views/users/single-user.vue'),
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/sessions',
+        name: 'SessionsAttended',
+        component: () => import(/* webpackChunkName: "sessions-attended" */ '@/router/views/sessions/attended-list.vue'),
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/sessions/taught',
+        name: 'SessionsTaught',
+        component: () => import(/* webpackChunkName: "sessions-taught" */ '@/router/views/sessions/taught-list.vue'),
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/sessions/:id',
+        name: 'Session',
+        component: () => import(/* webpackChunkName: "session" */ '@/router/views/sessions/single-session.vue'),
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/account',
+        name: 'Account',
+        component: () => import(/* webpackChunkName: "account" */ '@/router/views/account/index.vue'),
+        children: [
+            {
+                path: '/account',
+                name: 'AccountIndex',
+                component: () => import(/* webpackChunkName: "account_index" */ '@/router/views/account/account-index.vue'),
+            },
+            {
+                path: '/account/tests',
+                name: 'AccountTests',
+                component: () => import(/* webpackChunkName: "account_tests" */ '@/router/views/account/account-tests.vue'),
+            },
+            {
+                path: '/account/paymentMethods',
+                name: 'AccountPaymentMethods',
+                component: () => import(/* webpackChunkName: "account_payment_methods" */ '@/router/views/account/account-payment-methods.vue'),
+            },
+            {
+                path: '/account/transactions',
+                name: 'AccountTransactions',
+                component: () => import(/* webpackChunkName: "account_transactions" */ '@/router/views/account/account-transactions.vue'),
+            },
+        ],
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/posts',
+        name: 'Posts',
+        component: () => import(/* webpackChunkName: "posts" */ '@/router/views/posts/index.vue'),
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/ask-a-question',
+        name: 'AskQuestion',
+        component: () => import(/* webpackChunkName: "ask_question" */ '@/router/views/posts/ask-a-question.vue'),
+        meta: { requiresAuth: false }
+    },
+    {
+        path: '/posts/:id',
+        name: 'Post',
+        component: () => import(/* webpackChunkName: "post" */ '@/router/views/posts/single-post.vue'),
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/tests/tutors',
+        name: 'Tests Tutors',
+        component: () => import(/* webpackChunkName: "tests_tutors" */ '@/router/views/tests/tutors/index.vue'),
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/tests/tutors/:id',
+        name: 'Tests Tutor',
+        component: () => import(/* webpackChunkName: "tests_tutor" */ '@/router/views/tests/tutors/single-test.vue'),
+        meta: { requiresAuth: true }
+    },
+    {
+        path: '/admins',
+        name: 'Admins',
+        component: () => import(/* webpackChunkName: "admins" */ '@/router/views/admin/index.vue'),
+        children: [
+            {
+                path: '/admins/upgrades',
+                name: 'Upgrades',
+                component: () => import(/* webpackChunkName: "upgrades" */ '@/router/views/admin/upgrades/index.vue'),
+            },
+            {
+                path: '/admins/questions',
+                name: 'Questions',
+                component: () => import(/* webpackChunkName: "questions" */ '@/router/views/admin/questions/index.vue'),
+            },
+            {
+                path: '/admins/questions/:subject',
+                name: 'QuestionSubject',
+                component: () => import(/* webpackChunkName: "question-subject" */ '@/router/views/admin/questions/single-subject.vue'),
+            },
+            {
+                path: '/admins/questions/:subject/:module',
+                name: 'QuestionModule',
+                component: () => import(/* webpackChunkName: "question-module" */ '@/router/views/admin/questions/single-module.vue'),
+            },
+            {
+                path: '/admins/subjects',
+                name: 'Subjects',
+                component: () => import(/* webpackChunkName: "subjects" */ '@/router/views/admin/subjects/index.vue'),
+            },
+            {
+                path: '/admins/notes',
+                name: 'Notes',
+                component: () => import(/* webpackChunkName: "notes" */ '@/router/views/admin/notes/index.vue'),
+            }
+        ],
+        meta: { requiresAuth: true, requiresAdmin: true }
+    },
+    {
+        path: '*',
+        name: 'Not Found',
+        component: () => import(/* webpackChunkName: "not_found" */ '@/router/views/errors/not_found.vue')
+    }
+]
