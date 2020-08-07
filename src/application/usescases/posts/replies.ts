3b2b407b40d1d5861304f848a94a74265cd9bb0b
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-08-07 13:37:16 +0100

    Added listener in replies usecase

--- src/application/usescases/posts/replies.ts
+++ src/application/usescases/posts/replies.ts
@@ -1,14 +1,16 @@
 import ReplyEntity from '@root/modules/posts/domain/entities/replies'
 import { computed, reactive } from '@vue/composition-api'
-import { GetReplies } from '@root/modules/posts'
+import { GetReplies, ListenToReplies } from '@root/modules/posts'
 
 const PAGINATION_LIMIT = process.env.VUE_APP_PAGINATION_LIMIT
 const globalState: { [key: string]: {
+	fetched: boolean,
 	loading: boolean,
 	error: string,
 	replies: ReplyEntity[] ,
 	hasMore: boolean,
-	olderRepliesLoading: boolean
+	olderRepliesLoading: boolean,
+	listener: () => void
 } } = {}
 
 const fetchReplies = async (postId: string) => {
@@ -20,20 +22,36 @@ const fetchReplies = async (postId: string) => {
 	globalState[postId].replies = globalState[postId] ? globalState[postId].replies.concat(entities) : entities
 }
 
+const startListener = async (postId: string) => {
+	const postReplies = globalState[postId].replies
+	const lastIndex = postReplies.length - 1
+	const date = postReplies[lastIndex]?.createdAt
+	const addReplies = (entities: ReplyEntity[]) => {
+		entities.forEach(entity => {
+			const index = globalState[postId].replies.findIndex(e => e.id === entity.id)
+			index !== -1 ? globalState[postId].replies[index] = entity : globalState[postId].replies.unshift(entity)
+		})
+	}
+	globalState[postId].listener = await ListenToReplies.call(postId, addReplies, date)
+}
+
 export const useReplies = (postId: string) => {
 	if(globalState[postId] === undefined){
 		globalState[postId] = reactive({
+			fetched: false,
 			loading: false,
 			replies: [] as ReplyEntity[],
 			error: '',
 			hasMore: false,
-			olderRepliesLoading: false
+			olderRepliesLoading: false,
+			listener: () => {},
 		})
 	}
 
-	const fetchRepliesOnInit = async () => {
+	const fetchRepliesAndStartListenerOnInit = async () => {
 		globalState[postId].loading = true
 		await fetchReplies(postId).catch(() => globalState[postId].error = 'Error fetching replies')
+		await startListener(postId).catch(() => globalState[postId].error = 'Error starting listener')
 		globalState[postId].loading = false
 	}
 	const fetchOlderReplies = async () => {
@@ -42,11 +60,18 @@ export const useReplies = (postId: string) => {
 		globalState[postId].olderRepliesLoading = false
 	}
 
+	fetchRepliesAndStartListenerOnInit().then(() => globalState[postId].fetched = true)
+
 	return {
+		fetched: computed(() => globalState[postId].fetched),
 		loading: computed(() => globalState[postId].loading),
 		error: computed(() => globalState[postId].error),
 		replies: computed(() => globalState[postId].replies.reverse()),
 		hasMore: computed(() => globalState[postId].hasMore),
 		olderRepliesLoading: computed(() => globalState[postId].olderRepliesLoading),
+
+		fetchOlderReplies,
+		startListener: () => startListener(postId),
+		closeListener: globalState[postId].listener()
 	}
 }
