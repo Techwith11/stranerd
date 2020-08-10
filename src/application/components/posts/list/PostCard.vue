b0d85bed7f7e5b45ca5fe2c26b9b2e8e4755ef8d
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-08-10 20:51:03 +0100

    User in postcard

--- src/application/components/posts/list/PostCard.vue
+++ src/application/components/posts/list/PostCard.vue
@@ -8,9 +8,9 @@
 		<p class="card-text">{{ post.trimmedBody }}</p>
 		<router-link :to="`/posts/${post.id}`" class="card-link">See all replies</router-link>
 		<div class="d-flex align-items-center my-3">
-			<img :src="getImageLink" alt="" class="profile-image" id="ownerImage">
+			<img :src="user.image" alt="" class="profile-image" id="ownerImage">
 			<div>
-				<h6 class="mb-0">{{ getName }}</h6>
+				<h6 class="mb-0">{{ user.name }}</h6>
 				<p class="mb-0 small">Posted {{ post.createdDate }}</p>
 			</div>
 		</div>
@@ -18,10 +18,9 @@
 </template>
 
 <script lang="ts">
-	import { firestore } from '@/config/firebase'
 	import PostEntity from '@root/modules/posts/domain/entities/posts'
-    import { computed, defineComponent, reactive } from '@vue/composition-api'
-    import store from '@/store'
+	import { defineComponent, reactive, computed } from '@vue/composition-api'
+	import { fetchUser } from '@/usescases/users/users'
 	export default defineComponent({
 		props: {
 			post: {
@@ -30,11 +29,9 @@
 			}
 		},
 		setup(props){
-			let user: any = reactive({})
-            firestore.collection('users').doc(props.post.userId).get().then(doc => user = doc.data())
-            const getName = computed(() => user.bio?.name ?? '')
-            const getImageLink = computed(() => user.bio?.image?.link ?? store.getters.getDefaultImage)
-            return { user, getName, getImageLink }
+			const state = reactive({ user: {} })
+            fetchUser(props.post.userId).then(u => state.user = u as object)
+            return { user: computed(() => state.user) }
 		}
 	})
 </script>
