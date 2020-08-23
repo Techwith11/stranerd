c9702028e907318585101c455c2f82e8ddb6c62a
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-08-23 04:32:23 +0100

    Replies setup complete

--- src/application/router/views/posts/single-post.vue
+++ src/application/router/views/posts/single-post.vue
@@ -7,10 +7,6 @@
 				<div class="my-3">
 					<p class="text-muted" v-if="replies.length === 0">No replies yet. Consider leaving one</p>
 					<div v-else>
-						<div class="text-center small text-muted my-2" v-if="hasMore">
-							<i class="fas text-info fa-spinner fa-spin mr-1" v-if="olderRepliesLoading"></i>
-							<span @click="fetchOlderReplies">Fetch Older Replies</span>
-						</div>
 						<div class="pl-4">
 							<reply-card v-for="reply in replies" :key="reply.id" :reply="reply" :post="post"/>
 						</div>
@@ -23,7 +19,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, onUnmounted } from '@vue/composition-api'
+import { defineComponent, onUnmounted } from '@vue/composition-api'
 import PostInfo from '@/components/posts/single/PostInfo.vue'
 import ReplyForm from '@/components/posts/single/ReplyForm.vue'
 import ReplyCard from '@/components/posts/single/ReplyCard.vue'
@@ -36,16 +32,12 @@ export default defineComponent({
 		const { id } = router.currentRoute.params
 		const { post, loading, error, user } = usePost(id)
 		const {
-			replies, loading: replyLoading, fetched, error: replyError,
-			hasMore, olderRepliesLoading, fetchOlderReplies,
-			startListener, closeListener,
+			replies, loading: replyLoading, error: replyError, closeListener,
 		} = useReplies(id)
-		onMounted(() => fetched.value ? startListener() : null)
 		onUnmounted(closeListener)
 		return {
 			post, loading, error, user,
-			replies, replyLoading, fetched, replyError,
-			hasMore, olderRepliesLoading, fetchOlderReplies
+			replies, replyLoading, replyError
 		}
 	},
 	components: {
--- src/application/usecases/posts/replies.ts
+++ src/application/usecases/posts/replies.ts
@@ -1,20 +1,16 @@
 import { ReplyEntity } from '@root/modules/posts/domain/entities/reply'
 import { computed, reactive, watch } from '@vue/composition-api'
-import { AddReply, DownvoteReply, GetReplies, GetReplyFactory, ListenToReplies, UpvoteReply } from '@root/modules/posts'
+import { AddReply, DownvoteReply, GetReplyFactory, ListenToReplies, UpvoteReply } from '@root/modules/posts'
 import { Notify } from '@/config/notifications'
 import { fetchUser } from '@/usecases/users/users'
 import { UserEntity } from '@root/modules/users/domain/entities/user'
 import { FirestoreService } from '@root/modules/core/services/firebase'
 import { useStore } from '@/usecases/store'
 
-const PAGINATION_LIMIT = process.env.VUE_APP_PAGINATION_LIMIT
 const repliesGlobalState: { [key: string]: {
-	fetched: boolean,
 	loading: boolean,
 	error: string,
 	replies: ReplyEntity[],
-	hasMore: boolean,
-	olderRepliesLoading: boolean,
 	listener: () => void
 } } = {}
 const votesAndUsersGlobalState: { [key: string]: {
@@ -24,29 +20,14 @@ const votesAndUsersGlobalState: { [key: string]: {
 	user: UserEntity | undefined
 }} = {}
 
-const fetchReplies = async (postId: string) => {
-	const postReplies = repliesGlobalState[postId].replies
-	const lastIndex = postReplies.length - 1
-	const date = postReplies[lastIndex]?.createdAt
-	const entities = await GetReplies.call(postId, date)
-	repliesGlobalState[postId].hasMore = entities.length === PAGINATION_LIMIT
-	entities.forEach((entity) => {
-		const index = repliesGlobalState[postId].replies.findIndex((e) => e.id === entity.id)
-		index !== -1 ? repliesGlobalState[postId].replies[index] = entity : repliesGlobalState[postId].replies.unshift(entity)
-	})
-}
-
 const startListener = async (postId: string) => {
-	const postReplies = repliesGlobalState[postId].replies
-	const lastIndex = postReplies.length - 1
-	const date = postReplies[lastIndex]?.createdAt
 	const addReplies = (entities: ReplyEntity[]) => {
 		entities.forEach((entity) => {
 			const index = repliesGlobalState[postId].replies.findIndex((e) => e.id === entity.id)
 			index !== -1 ? repliesGlobalState[postId].replies[index] = entity : repliesGlobalState[postId].replies.push(entity)
 		})
 	}
-	repliesGlobalState[postId].listener = await ListenToReplies.call(postId, addReplies, date)
+	repliesGlobalState[postId].listener = await ListenToReplies.call(postId, addReplies)
 }
 
 export const useReplies = (postId: string) => {
@@ -62,33 +43,18 @@ export const useReplies = (postId: string) => {
 		})
 	}
 
-	const fetchRepliesAndStartListenerOnInit = async () => {
+	const startListenerOnInit = async () => {
 		repliesGlobalState[postId].loading = true
-		if(!repliesGlobalState[postId].fetched){
-			await fetchReplies(postId).catch(() => repliesGlobalState[postId].error = 'Error fetching replies')
-			repliesGlobalState[postId].fetched = true
-		}
 		await startListener(postId).catch(() => repliesGlobalState[postId].error = 'Error starting listener')
 		repliesGlobalState[postId].loading = false
 	}
-	const fetchOlderReplies = async () => {
-		repliesGlobalState[postId].olderRepliesLoading = true
-		await fetchReplies(postId).catch(() => repliesGlobalState[postId].error = 'Error fetching older replies')
-		repliesGlobalState[postId].olderRepliesLoading = false
-	}
 
-	fetchRepliesAndStartListenerOnInit().catch(() => repliesGlobalState[postId].error = 'Unknown error')
+	startListenerOnInit().catch(() => repliesGlobalState[postId].error = 'Unknown error')
 
 	return {
-		fetched: computed(() => repliesGlobalState[postId].fetched),
 		loading: computed(() => repliesGlobalState[postId].loading),
 		error: computed(() => repliesGlobalState[postId].error),
 		replies: computed(() => repliesGlobalState[postId].replies),
-		hasMore: computed(() => repliesGlobalState[postId].hasMore),
-		olderRepliesLoading: computed(() => repliesGlobalState[postId].olderRepliesLoading),
-
-		fetchOlderReplies,
-		startListener: () => startListener(postId),
 		closeListener: () => repliesGlobalState[postId].listener()
 	}
 }
