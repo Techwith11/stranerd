407c6f71999da3c15872f8b65ace21dfb43f3a3a
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-08-31 07:53:59 +0100

    content functions

---
+++ firebase/functions/src/onCall/contents/createPost.ts
@@ -0,0 +1,39 @@
+import functions from 'firebase-functions'
+import admin from 'firebase-admin'
+import { isProduction } from '../../helpers/environment'
+
+export default functions.https.onCall(async ({ post }, context) => {
+	if (isProduction && !context.auth) {
+		throw new functions.https.HttpsError('unauthenticated', 'Only authenticated users can create posts')
+	}
+	try{
+		const id = post.userId
+		const doc = await admin.firestore().collection('users').doc(id).get()
+		const user = doc.data()
+		if(user?.account?.questions > 0){
+			let questions = user?.account.questions
+			questions--
+			post.dates = { createdAt: admin.firestore.FieldValue.serverTimestamp() }
+			try{
+				const batch = admin.firestore().batch()
+				const postRef = admin.firestore().collection('posts').doc()
+				batch.set(postRef, post)
+				const userRef = admin.firestore().collection('users').doc(id)
+				batch.update(userRef, 'account.questions', questions)
+				await batch.commit()
+
+				return postRef.id
+			}catch(error){
+				throw new functions.https.HttpsError('unknown', error.message)
+			}
+		}else{
+			if(user?.account?.subscription?.id){
+				throw new functions.https.HttpsError('failed-precondition', 'You do not have any free questions left. Consider buying more questions from the shop')
+			}else {
+				throw new functions.https.HttpsError('failed-precondition','You do not have any free questions left. Consider upgrading to a paid subscription to ask more questions')
+			}
+		}
+	}catch(error){
+		throw new functions.https.HttpsError('unknown', error.message)
+	}
+})
---
+++ firebase/functions/src/onCall/contents/downvoteReply.ts
@@ -0,0 +1,33 @@
+import functions from 'firebase-functions'
+import admin from 'firebase-admin'
+import { isProduction } from '../../helpers/environment'
+
+export default functions.https.onCall(async ({ id, user, post, reply }, context) => {
+	if (isProduction && !context.auth) {
+		throw new functions.https.HttpsError('unauthenticated', 'Only authenticated users can downvote replies')
+	}
+
+	if (id === user) {
+		throw new functions.https.HttpsError('failed-precondition', 'You cannot downvote your own replies')
+	}
+	try{
+		await admin.database().ref(`posts/${post}/replies/${reply}/votes/${id}`).set(false)
+
+		const userRef = await admin.firestore().collection('users').doc(user)
+		const userData = (await userRef.get()).data()
+		if(userData?.roles?.isTutor){
+			const { rating, reviews } = userData.tutor
+			const overall = ((rating * reviews) - 5) / (reviews - 1)
+			await userRef.set({
+				tutor: {
+					rating: parseFloat(overall.toFixed(2)),
+					reviews: reviews - 1
+				}
+			}, { merge: true })
+		}
+
+		return true
+	}catch(error){
+		throw new functions.https.HttpsError('unknown', error.message)
+	}
+})
