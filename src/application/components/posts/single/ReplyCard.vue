17ccf027a3e03adc930154203766aab026dbcfaf
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-10-15 05:41:25 +0100

    UI of reply card

--- src/application/components/posts/single/ReplyCard.vue
+++ src/application/components/posts/single/ReplyCard.vue
@@ -1,22 +1,26 @@
 <template>
-	<div class="mb-4 shadow-sm p-3 rounded-xl" v-if="!loading">
-		<div class="d-flex align-items-center mb-2">
+	<div class="mb-5" v-if="!loading">
+		<div class="d-flex align-items-start mb-2">
 			<img :src="user.image" class="profile-image" id="ownerImage" alt="">
-			<div class="small">
-				<p class="mb-0"><router-link class="text-dark" :to="`/users/${user.id}`">{{ user.name }}</router-link></p>
-				<p class="mb-0">on {{ reply.createdDate }}</p>
-			</div>
-		</div>
-		<div v-html="reply.body" class="editor-container"></div>
-		<div>
-			<div class="my-2 d-flex align-items-center small">
-				<button class="btn btn-success flex-grow-1 mr-2" @click.prevent="likeReply" :disabled="cannotLike">Like</button>
-				<button class="btn btn-danger flex-grow-1" @click.prevent="dislikeReply" :disabled="cannotDislike">Dislike</button>
-			</div>
-			<div>
-				<span class="mr-2" v-if="reply.likesCount">{{ reply.likesCount }} {{ reply.likesCount === 1 ? 'like' : 'likes' }}</span>
-				<span class="mr-2" v-if="reply.dislikesCount">{{ reply.dislikesCount }} {{ reply.dislikesCount === 1 ? 'dislike' : 'dislikes' }}</span>
-				<i class="fas fa-spinner fa-spin text-info mr-2" v-if="liking"></i>
+			<div class="flex-grow-1">
+				<div class="small">
+					<p class="mb-0"><router-link class="text-dark" :to="`/users/${user.id}`">{{ user.name }}</router-link></p>
+					<p class="mb-0">on {{ reply.createdDate }}</p>
+				</div>
+				<div class="shadow-sm rounded p-3">
+					<div v-html="reply.body" class="editor-container"></div>
+				</div>
+				<div>
+					<div class="d-flex align-items-center small">
+						<button class="btn btn-success shadow-sm flex-grow-1 rounded-0 text-wrap" @click.prevent="likeReply" :disabled="cannotLike">
+							{{ reply.likesCount }} {{ reply.likesCount === 1 ? 'Like' : 'Likes' }}
+						</button>
+						<button class="btn btn-danger shadow-sm flex-grow-1 rounded-0 text-wrap" @click.prevent="dislikeReply" :disabled="cannotDislike">
+							{{ reply.dislikesCount }} {{ reply.dislikesCount === 1 ? 'Unlike' : 'Unlikes' }}
+						</button>
+					</div>
+					<i class="fas fa-spinner fa-spin text-info mr-2" v-if="liking"></i>
+				</div>
 			</div>
 		</div>
 	</div>
